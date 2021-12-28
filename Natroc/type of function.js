6e70bd063ba07794680ade549863de6da3363f7a
Karthickkms <96235067+Karthickkms@users.noreply.github.com>
2021-12-28 20:50:39 +0530

    tasks

---
+++ Natroc/type of function.js
@@ -0,0 +1,36 @@
+//1)MOVIE LIST:
+const Names = [{ Movie : "Casino Royale"},{ Studio : "Eon Productions"},{Ratings : "PG13" }];
+
+console.log(Names);
+ 
+ 
+ 
+// 2) PERSON : 
+ var student = {
+     firstName:"Karthikeyan",
+     lastName: "David",
+     fullName: function () {
+         return this.lastName + ", " + this.firstName;
+     },
+};
+
+student.fullName()
+console.log(student.fullName());
+
+
+
+
+
+//3) UBERPRICE:
+var uberprice = {
+    "localized_display_name":"uberXL",
+    "distance": 6.17,
+    "display_name":"uberXL",
+    "product_id":20245-36,
+    "high_estimate": 25,
+    "low_estimate": 20,
+    "duration": 1080,
+    "estimate price":"50-100",
+}
+console.log(uberprice);
+
